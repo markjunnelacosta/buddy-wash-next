618aa5312d2c71d77f071fec93fd2f26050d2410
Levenie <ailalevenieacosta123@gmail.com>
2023-10-05 17:50:50 +0800

    Added UI for Machines (staff View)

--- app/role/staff/page.jsx
+++ app/role/staff/page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../../../components/navBar/staff/navBar";
 import Dashboard from "../../../components/main-content/staff/dashBoard/Dashboard";
 import LaundryBin from "@/components/main-content/staff/laundryBin/LaundryBin";
+import Machines from "@/components/main-content/staff/machine/Machines";
 const Staff = () => {
   return(
     <div>
       {/* <NavBar />
       <Dashboard /> */}
       {/* < LaundryBin /> */}
+      {/* <Machines /> */}
     </div>
   )
 
