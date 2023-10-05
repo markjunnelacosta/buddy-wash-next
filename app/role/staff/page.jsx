eeb5a2b72dc87a4d21564aee9d2f35914be3836e
Levenie <ailalevenieacosta123@gmail.com>
2023-10-05 17:09:17 +0800

    Added UI for dashboard(staff View)

--- app/role/staff/page.jsx
+++ app/role/staff/page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NavBar from "../../../components/navBar/staff/navBar";
-
+import Dashboard from "../../../components/main-content/staff/dashBoard/Dashboard";
 const Staff = () => {
   return(
     <div>
-      <NavBar />
+      {/* <NavBar />
+      <Dashboard /> */}
     </div>
   )
 
