cce69b773be036807a5de3ccfcd6cec52a7f1262
Levenie <ailalevenieacosta123@gmail.com>
2023-10-05 17:59:11 +0800

    Added UI for Manage Customer (staff view)

--- app/role/staff/page.jsx
+++ app/role/staff/page.jsx
@@ -3,6 +3,9 @@ import NavBar from "../../../components/navBar/staff/navBar";
 import Dashboard from "../../../components/main-content/staff/dashBoard/Dashboard";
 import LaundryBin from "@/components/main-content/staff/laundryBin/LaundryBin";
 import Machines from "@/components/main-content/staff/machine/Machines";
+import ManageCustomer from "@/components/main-content/staff/manageCustomer/ManageCustomer";
+
+
 const Staff = () => {
   return(
     <div>
@@ -10,6 +13,7 @@ const Staff = () => {
       <Dashboard /> */}
       {/* < LaundryBin /> */}
       {/* <Machines /> */}
+      {/* < ManageCustomer /> */}
     </div>
   )
 
