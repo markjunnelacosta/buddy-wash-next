fa3aa1a917d59a3543bf5068d511f1141ef5c1c9
Levenie <ailalevenieacosta123@gmail.com>
2023-10-05 17:41:38 +0800

    Added UI for LaundryBin (staff View)

--- app/role/staff/page.jsx
+++ app/role/staff/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../../../components/navBar/staff/navBar";
 import Dashboard from "../../../components/main-content/staff/dashBoard/Dashboard";
+import LaundryBin from "@/components/main-content/staff/laundryBin/LaundryBin";
 const Staff = () => {
   return(
     <div>
       {/* <NavBar />
       <Dashboard /> */}
+      {/* < LaundryBin /> */}
     </div>
   )
 
