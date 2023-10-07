fbfc36f232d9aee70594f36ca3991f1a4df8e1a6
Katrish <zatekatrish@gmail.com>
2023-10-08 00:46:37 +0700

    branch1 pages

---
+++ app/role/owner/components/main/branch1/page.jsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import "./branch1.css";
+import {
+  List,
+  Divider,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Grid,
+  Paper,
+} from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleIcon from "@mui/icons-material/People";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import LayersIcon from "@mui/icons-material/Layers";
+//import Counter from "./Counter";
+//import Chart from "./Chart";
+
+const Dashboard = () => {
+  return (
+    <div className="branch1-container">
+    
+      <div className="graphs-container">
+        <div className="top-container">
+          <div className="counters-container">
+            
+          </div>
+          <div className="customers-container">
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 240,
+                }}
+              >
+              </Paper>
+            </Grid>
+          </div>
+        </div>
+        <div className="bar-container"></div>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
