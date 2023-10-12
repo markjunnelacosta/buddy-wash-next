644cb231bed0935d6fb147cd7621424174fb973d
Levenie <ailalevenieacosta123@gmail.com>
2023-10-13 00:25:50 +0800

    added customers from db to the table (staff view)

--- app/api/customer/route.js
+++ app/api/customer/route.js
@@ -1,29 +1,32 @@
 import { connectToDB } from "@/utils/database";
 import Customer from "@/models/customer";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-export const GET = async (req, res) => {
-  try {
-    await connectToDB();
-    const customer = await customer.find({}).populate("customerName");
-    return new Response(JSON.stringify(customer), { status: 200 });
-  } catch (error) {
-    return new Response("Failed get customer", { status: 500 });
-  }
-};
+// export const GET = async (req, res) => {
+//   try {
+//     await connectToDB();
+//     const customers = await Customer.find({}).populate("customerName");
+//     return new Response(JSON.stringify(customers), { status: 200 });
+//   } catch (error) {
+//     return new Response("Failed get customer", { status: 500 });
+//   }
+// };
 
+export async function GET() {
+  await connectToDB();
+  const customers = await Customer.find();
+  return NextResponse.json({ customers });
+}
 
 export const POST = async (req) => {
   const body = await req.json();
   const { customerName, customerNumber } = body;
- 
+
   try {
     await connectToDB();
     const newCustomer = new Customer({
       customerName,
       customerNumber,
-      
-      
     });
     console.log(newCustomer);
     await newCustomer.save();
@@ -34,8 +37,11 @@ export const POST = async (req) => {
 };
 
 export async function DELETE(request) {
-  const id= request.nextUrl.searchParams.get("id");
+  const id = request.nextUrl.searchParams.get("id");
   await connectToDB();
   await Customer.findByIdAndDelete(id);
-  return NextResponse.json({message: "Deleted a customer Record"}, {status:201});
+  return NextResponse.json(
+    { message: "Deleted a customer Record" },
+    { status: 201 }
+  );
 }
--- app/role/components/forms/addCustomer/page.jsx
+++ app/role/components/forms/addCustomer/page.jsx
@@ -1,62 +1,67 @@
 "use client";
-import React, { useState } from 'react';
-import './AddCustomer.css';
+import React, { useState } from "react";
+import "./AddCustomer.css";
+import { Button } from "@mui/material";
 
+const AddCustomer = () => {
+  const [customerName, setCustomerName] = useState("");
+  const [customerNumber, setCustomerNumber] = useState("");
 
+  const onClickSave = async () => {
+    console.log(customerName, customerNumber);
 
-const AddCustomer= () =>{
-    const [customerName, setCustomerName]= useState("");
-    const [customerNumber, setCustomerNumber]= useState("");
-
-    const onClickSave = async() => {
-        console.log(customerName, customerNumber);
-
-        const response = await fetch("/api/customer", {
-            method: "POST",
-            body: JSON.stringify({
-                customerName: customerName,
-                customerNumber: customerNumber,
-
-            }),
-        });
-        console.log(response);
-    };
-    return (
-        
-        <div className="add-customer-form">
-            <div className="new-customer-text">
-                <p>New Customer</p>
-            </div>
-            <div className="input">
-                <div className="customer-name"> 
-                    <p>Customer Name</p>
-                    <input 
-                    className="text-box"
-                    value={customerName}
-                    onChange={(e) => setCustomerName(e.currentTarget.value)} ></input>
-                </div>
-                <div className="phone-number"> 
-                    <p>Phone Number</p>
-                    <input 
-                    className="text-box"
-                    value={customerNumber}
-                    onChange={(e)=> setCustomerNumber(e.currentTarget.value)}></input> 
-                </div>
-            </div>
-            <div className="buttons">
-                <div >
-                    <button 
-                    className="button"
-                    onClick={onClickSave}>Save</button>
-                    
-                </div>
-                <div >
-                    <button className="button">Cancel</button>
-                </div>
-            </div>
+    const response = await fetch("/api/customer", {
+      method: "POST",
+      body: JSON.stringify({
+        customerName: customerName,
+        customerNumber: customerNumber,
+      }),
+    });
+    console.log(response);
+  };
+  return (
+    <div className="add-customer-form">
+      <div className="new-customer-text">
+        <p>New Customer</p>
+      </div>
+      <div className="input">
+        <div className="customer-name">
+          <p>Customer Name</p>
+          <input
+            className="text-box"
+            value={customerName}
+            onChange={(e) => setCustomerName(e.currentTarget.value)}
+          ></input>
         </div>
-        
-        
-    )
-}
+        <div className="phone-number">
+          <p>Phone Number</p>
+          <input
+            className="text-box"
+            value={customerNumber}
+            onChange={(e) => setCustomerNumber(e.currentTarget.value)}
+          ></input>
+        </div>
+      </div>
+      <div className="buttons">
+        <div>
+          <Button
+            href="/role/staff/components/main-content/manageCustomer"
+            className="button"
+            onClick={onClickSave}
+          >
+            Save
+          </Button>
+        </div>
+        <div>
+          <Button
+            href="/role/staff/components/main-content/manageCustomer"
+            className="button"
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default AddCustomer;
