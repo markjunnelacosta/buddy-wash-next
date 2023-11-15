0e749d53b6840fca82b369e0b00ba3e57e8b2ed2
Levenie <ailalevenieacosta123@gmail.com>
2023-11-15 19:26:18 +0800

    dryerToggle UI only

--- app/role/testingPage/page.jsx
+++ app/role/testingPage/page.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 // import App from "../staff/laundryBin/orderSummary";
 import MachineToggle from "../staff/laundryBin/machineToggle";
-
+import DryerToggle from "../staff/laundryBin/dryerToggle";
 const TestingPage = () => {
   return (
     <div>
       {/* <App /> */}
       <MachineToggle />
+      <DryerToggle />
     </div>
   );
 };
