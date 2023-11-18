802b131d131ad3fbd133d78799c3d35acd153bb0
Levenie <ailalevenieacosta123@gmail.com>
2023-11-18 09:45:52 +0800

    laundrybin machine timer. Not connected to db yet

--- app/role/staff/laundryBin/page.jsx
+++ app/role/staff/laundryBin/page.jsx
@@ -1,5 +1,178 @@
 "use client";
 
+// import React from "react";
+// import "./LaundryBin.css";
+// import { Button } from "@mui/material";
+// import AddIcon from "@mui/icons-material/Add";
+// import { Add, Timer } from "@mui/icons-material";
+// import { useState, useEffect } from "react";
+// import AddLaundry from "../../components/forms/addLaundry/page";
+// import Paper from "@mui/material/Paper";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import MachineToggle from "./machineToggle";
+// import DryerToggle from "./dryerToggle";
+
+// const getOrder = async () => {
+//   try {
+//     const res = await fetch("http://localhost:3000/api/laundrybin", {
+//       cache: "no-store",
+//     });
+
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch order");
+//     }
+
+//     const response = await res.json();
+//     return response.laundryData || [];
+//   } catch (error) {
+//     console.log("Error loading order: ", error);
+//   }
+// };
+
+// const LaundryBin = () => {
+//   const [laundryData, setLaundryData] = useState([]);
+//   const [showAddLaundry, setShowAddLaundry] = useState(false);
+
+//   const openAddLaundry = () => {
+//     setShowAddLaundry(true);
+//   };
+
+//   const closeAddLaundry = () => {
+//     setShowAddLaundry(false);
+//   };
+
+//   useEffect(() => {
+//     const fetchOrder = async () => {
+//       try {
+//         const order = await getOrder();
+//         setLaundryData(order);
+//       } catch (error) {
+//         console.error("Error fetching user:", error);
+//       }
+//     };
+
+//     fetchOrder();
+//   }, []);
+
+//   useEffect(() => {
+//     console.log(laundryData);
+//   }, [laundryData]);
+
+//   const fetchData = async () => {
+//     try {
+//       const res = await fetch("http://localhost:3000/api/laundrybin", {
+//         cache: "no-store",
+//       });
+
+//       if (!res.ok) {
+//         throw new Error("Failed to fetch order");
+//       }
+
+//       const response = await res.json();
+//       const order = response.laundryData || [];
+//       setLaundryData(order);
+//     } catch (error) {
+//       console.log("Error loading orders: ", error);
+//     }
+//   };
+
+//   const handleSaveData = () => {
+//     closeAddLaundry();
+//     fetchData();
+//   };
+//   return (
+//     <>
+//       <div className="laundryBin-container">
+//         <div className="blue-container">
+//           <Button
+//             style={{
+//               backgroundColor: "white",
+//               color: "black",
+//               width: "200px",
+//               height: "50px",
+//               margin: "0",
+//               fontWeight: "bold",
+//               alignSelf: "flex-end",
+//               margin: "30px",
+//               borderRadius: "10px",
+//             }}
+//             variant="contained"
+//             startIcon={<Add />}
+//             onClick={openAddLaundry}
+//           >
+//             New Laundry
+//           </Button>
+//           <div className="table-container">
+//             <TableContainer component={Paper}>
+//               <Table
+//                 sx={{ minWidth: 650 }}
+//                 size="small"
+//                 aria-label="a dense table"
+//               >
+//                 <TableHead>
+//                   <TableRow>
+//                     <TableCell className="table-header">Date </TableCell>
+//                     <TableCell className="table-header">Name</TableCell>
+//                     <TableCell className="table-header">Machine No. </TableCell>
+//                     <TableCell className="table-header">Action</TableCell>
+//                     <TableCell className="table-header">Timer</TableCell>
+//                     <TableCell className="table-header">Dryer No. </TableCell>
+//                     <TableCell className="table-header">Action</TableCell>
+//                     <TableCell className="table-header">Timer</TableCell>
+//                     <TableCell className="table-header">Status</TableCell>
+//                   </TableRow>
+//                 </TableHead>
+//                 <TableBody>
+//                   {laundryData.map((order) => (
+//                     <TableRow
+//                       key={order._id}
+//                       sx={{
+//                         "&:last-child td, &:last-child th": { border: 0 },
+//                       }}
+//                     >
+//                       <TableCell className="">
+//                         {new Date(order.orderDate).toLocaleDateString()}
+//                       </TableCell>
+//                       <TableCell className="">{order.customerName}</TableCell>
+//                       <TableCell className="">t</TableCell>
+//                       <TableCell className="">
+//                         <MachineToggle
+//                           onToggle={() => {
+//                             console.log("Start timer for" + order.customerName);
+//                           }}
+//                         />
+//                       </TableCell>
+//                       <TableCell className="">{/* <WashTimer /> */}</TableCell>
+//                       <TableCell className="">t</TableCell>
+//                       <TableCell className="">
+//                         <DryerToggle />
+//                       </TableCell>
+//                       <TableCell className="">00:00</TableCell>
+//                       <TableCell className="">t</TableCell>
+//                     </TableRow>
+//                   ))}
+//                 </TableBody>
+//               </Table>
+//             </TableContainer>
+//           </div>
+//         </div>
+//       </div>
+
+//       <AddLaundry
+//         isOpen={showAddLaundry}
+//         onClose={closeAddLaundry}
+//         onSaveData={handleSaveData}
+//       />
+//     </>
+//   );
+// };
+// export default LaundryBin;
+
 import React from "react";
 import "./LaundryBin.css";
 import { Button } from "@mui/material";
@@ -16,6 +189,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import MachineToggle from "./machineToggle";
 import DryerToggle from "./dryerToggle";
+import Countdown from "react-countdown";
 
 const getOrder = async () => {
   try {
@@ -37,6 +211,7 @@ const getOrder = async () => {
 const LaundryBin = () => {
   const [laundryData, setLaundryData] = useState([]);
   const [showAddLaundry, setShowAddLaundry] = useState(false);
+  const [machineTimer, setMachineTimer] = useState([]);
 
   const openAddLaundry = () => {
     setShowAddLaundry(true);
@@ -51,6 +226,12 @@ const LaundryBin = () => {
       try {
         const order = await getOrder();
         setLaundryData(order);
+        const timers = [];
+        order.forEach((o) => {
+          timers.push({ orderId: o._id, startTime: 0 });
+        });
+        console.log("timers", timers);
+        setMachineTimer(timers);
       } catch (error) {
         console.error("Error fetching user:", error);
       }
@@ -60,7 +241,7 @@ const LaundryBin = () => {
   }, []);
 
   useEffect(() => {
-    console.log(laundryData);
+    // console.log(laundryData);
   }, [laundryData]);
 
   const fetchData = async () => {
@@ -73,6 +254,7 @@ const LaundryBin = () => {
         throw new Error("Failed to fetch order");
       }
 
+      // create reference for order_id with timer
       const response = await res.json();
       const order = response.laundryData || [];
       setLaundryData(order);
@@ -85,6 +267,68 @@ const LaundryBin = () => {
     closeAddLaundry();
     fetchData();
   };
+
+  const computedDate = (start) => {
+    const startdate = new Date(start);
+    if (startdate) {
+      const endDate = startdate;
+      endDate.setMinutes(startdate.getMinutes() + 30);
+
+      const currDate = new Date();
+
+      console.log(startdate.toLocaleTimeString(), "Start");
+      console.log(endDate.toLocaleTimeString(), "End");
+      console.log(currDate.toLocaleTimeString(), "Current Time");
+      console.log(endDate.toLocaleTimeString(), "Remaining");
+      return endDate.getTime() - currDate.getTime();
+    }
+
+    return 0;
+  };
+
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      //Render a complete state
+      <span>Done</span>;
+    } else {
+      //Render a countdown
+      return (
+        <span>
+          {minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+
+  useEffect(() => {
+    console.log(machineTimer);
+  }, [machineTimer]);
+
+  const onToggleChange = (orderId) => {
+    const timers = [...machineTimer];
+    const index = machineTimer.findIndex((m) => m.orderId == orderId);
+    timers[index].startTime = Date.now();
+
+    setMachineTimer(timers);
+  };
+
+  const getOrderStartTime = (orderId) => {
+    const index = machineTimer.findIndex((m) => m.orderId == orderId);
+    return machineTimer[index].startTime;
+  };
+
+  const getCountDownTimer = (orderId) => {
+    const startTime = getOrderStartTime(orderId);
+    if (startTime) {
+      return (
+        <Countdown
+          date={Date.now() + computedDate(getOrderStartTime(orderId))}
+          renderer={renderer}
+        />
+      );
+    }
+  };
+
   return (
     <>
       <div className="laundryBin-container">
@@ -142,12 +386,12 @@ const LaundryBin = () => {
                       <TableCell className="">t</TableCell>
                       <TableCell className="">
                         <MachineToggle
-                          onToggle={() => {
-                            console.log("Start timer for" + order.customerName);
-                          }}
+                          onToggle={() => onToggleChange(order._id)}
                         />
                       </TableCell>
-                      <TableCell className="">{/* <WashTimer /> */}</TableCell>
+                      <TableCell className="">
+                        {getCountDownTimer(order._id)}
+                      </TableCell>
                       <TableCell className="">t</TableCell>
                       <TableCell className="">
                         <DryerToggle />
