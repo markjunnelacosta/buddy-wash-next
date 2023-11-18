7a46fb4c8dc58c5fbf5359132a2e856bf95e7efb
Katrish <zatekatrish@gmail.com>
2023-11-18 22:49:17 +0700

    updates

--- app/role/owner/components/main/dashboard/page.jsx
+++ app/role/owner/components/main/dashboard/page.jsx
@@ -10,6 +10,9 @@ const Dashboard = () => {
   const [reportData, setReportData] = useState([]);
   const [totalProfit, setTotalProfit] = useState(0);
   const [customerCount, setCustomerCount] = useState(0);
+  const [b1Profit, setB1Profit] = useState(0);
+  const [b2Profit, setB2Profit] = useState(0);
+  const [b3Profit, setB3Profit] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,6 +32,27 @@ const Dashboard = () => {
           data.reportData.map((report) => report.customerName)
         );
         setCustomerCount(uniqueCustomers.size);
+
+        const b1ProfitValue = data.reportData.reduce(
+          (acc, report) => acc + report.b1Amount,
+          0
+        );
+        console.log("B1 Profit:", b1ProfitValue);
+        setB1Profit(b1ProfitValue);
+
+        const b2ProfitValue = data.reportData.reduce(
+          (acc, report) => acc + report.b2Amount,
+          0
+        );
+        console.log("B2 Profit:", b2ProfitValue);
+        setB2Profit(b2ProfitValue);
+
+        const b3ProfitValue = data.reportData.reduce(
+          (acc, report) => acc + report.b3Amount,
+          0
+        );
+        console.log("B3 Profit:", b3ProfitValue);
+        setB3Profit(b3ProfitValue);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -42,13 +66,14 @@ const Dashboard = () => {
     const averageTotalAmount =
       pastReports.reduce((acc, pastReport) => acc + pastReport.totalAmount, 0) /
       (pastReports.length || 1);
-      return {
-        forecastDate: report.reportDate,
-        forecastedAmount: averageTotalAmount,
-      };
-    });
 
-    console.log("Forecast Data:", forecastData);
+    return {
+      forecastDate: report.reportDate,
+      forecastedAmount: averageTotalAmount,
+    };
+  });
+
+  console.log("Forecast Data:", forecastData);
 
   return (
     <div className="dashboard-container">
@@ -58,6 +83,12 @@ const Dashboard = () => {
             <Counter title="Total Profit Today" value={totalProfit.toFixed(2)} />
             <Counter title="Customers Today" value={customerCount} />
           </div>
+          <div className="counters-container1">
+            <Counter title="B1 Profit Today" value={b1Profit.toFixed(2)} />
+            <Counter title="B2 Profit Today" value={b2Profit.toFixed(2)} />
+            <Counter title="B3 Profit Today" value={b3Profit.toFixed(2)} />
+          </div>
+
           <div className="customers-container">
             <Grid item xs={12} md={8} lg={9}>
               <Paper
