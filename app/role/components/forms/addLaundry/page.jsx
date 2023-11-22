0e3c192de85378ea043481ee44951b9ce0fc0102
Christine <christinebayas@gmail.com>
2023-11-22 23:46:09 +0800

    laundrybin-supply

--- app/role/components/forms/addLaundry/page.jsx
+++ app/role/components/forms/addLaundry/page.jsx
@@ -4,15 +4,9 @@ import "./addLaundry.css";
 import { Select } from "@mui/material";
 import { Autocomplete, TextField } from "@mui/material";
 
-<<<<<<< HEAD
-const AddLaundry = ({ isOpen, onClose, onSaveData, }) => {
-    const [customerData, setCustomerData] = useState([]); // State for customers
-    const [supplyData, setSupplyData] = useState([]); // State for supplies
-=======
 const AddLaundry = ({ isOpen, onClose, onSaveData, onUpdateSupply }) => {
   const [customerData, setCustomerData] = useState([]); // State for customers
   const [supplyData, setSupplyData] = useState([]); // State for supplies
->>>>>>> b3f645f4829de7be79e17a9414c11da13e90ef1e
 
   const [customerName, setCustomerName] = useState("");
   const [orderDate, setOrderDate] = useState("");
@@ -186,28 +180,41 @@ const AddLaundry = ({ isOpen, onClose, onSaveData, onUpdateSupply }) => {
     console.log(res);
     console.log("orderszzzzzz" + orderRes);
 
-    const selectedDetergent = filterSuppliesByKeyword(
-      supplyData,
-      "detergent"
-    ).find((supply) => supply.supplyName === detergent);
-    const selectedFabCon = filterSuppliesByKeyword(
-      supplyData,
-      "conditioner"
-    ).find((supply) => supply.supplyName === fabCon);
+    // Update available stock for selected detergent and fabric conditioner
+    if (detergent && detergentQty) {
+      const selectedDetergent = supplyData.find((supply) => supply.supplyName === detergent);
 
-    if (onUpdateSupply) {
-      onUpdateSupply({
-        detergentData: {
-          supplyId: selectedDetergent._id,
-          quantity: detergentQty,
-          type: "Out",
-        },
-        fabConData: {
-          supplyId: selectedFabCon._id,
-          quantity: fabConQty,
-          type: "Out",
-        },
-      });
+      if (selectedDetergent) {
+        const supplyUpdateResponse = await fetch(`/api/laundry-supply/${encodeURIComponent(selectedDetergent._id)}`, {
+          method: "PATCH",
+          body: JSON.stringify({
+            quantity: detergentQty, // Subtract the used quantity
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        console.log(supplyUpdateResponse);
+      }
+    }
+
+    if (fabCon && fabConQty) {
+      const selectedFabCon = supplyData.find((supply) => supply.supplyName === fabCon);
+
+      if (selectedFabCon) {
+        const supplyUpdateResponse = await fetch(`/api/laundry-supply/${encodeURIComponent(selectedFabCon._id)}`, {
+          method: "PATCH",
+          body: JSON.stringify({
+            quantity: fabConQty, // Subtract the used quantity
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        console.log(supplyUpdateResponse);
+      }
     }
 
     onSaveData();
@@ -221,60 +228,6 @@ const AddLaundry = ({ isOpen, onClose, onSaveData, onUpdateSupply }) => {
           cache: "no-store",
         });
 
-<<<<<<< HEAD
-        const res = await fetch("/api/report", {
-            method: "POST",
-            body: JSON.stringify({
-                customerName: customerName,
-                reportDate: orderDate,
-                totalAmount: totalAmount
-            }),
-        });
-
-        console.log(response);
-        console.log(res);
-
-        // Update available stock for selected detergent and fabric conditioner
-        if (detergent && detergentQty) {
-            const selectedDetergent = supplyData.find((supply) => supply.supplyName === detergent);
-
-            if (selectedDetergent) {
-                const supplyUpdateResponse = await fetch(`/api/laundry-supply/${encodeURIComponent(selectedDetergent._id)}`, {
-                    method: "PATCH",
-                    body: JSON.stringify({
-                        quantity: detergentQty, // Subtract the used quantity
-                    }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-
-                console.log(supplyUpdateResponse);
-            }
-        }
-
-        if (fabCon && fabConQty) {
-            const selectedFabCon = supplyData.find((supply) => supply.supplyName === fabCon);
-
-            if (selectedFabCon) {
-                const supplyUpdateResponse = await fetch(`/api/laundry-supply/${encodeURIComponent(selectedFabCon._id)}`, {
-                    method: "PATCH",
-                    body: JSON.stringify({
-                        quantity: fabConQty, // Subtract the used quantity
-                    }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-
-                console.log(supplyUpdateResponse);
-            }
-        }
-
-
-        onSaveData();
-        onClose();
-=======
         if (!res.ok) {
           throw new Error("Failed to fetch customer");
         }
@@ -284,231 +237,14 @@ const AddLaundry = ({ isOpen, onClose, onSaveData, onUpdateSupply }) => {
       } catch (error) {
         console.error("Error fetching customer:", error);
       }
->>>>>>> b3f645f4829de7be79e17a9414c11da13e90ef1e
     };
 
     fetchCustomer();
   }, []);
 
-<<<<<<< HEAD
-    useEffect(() => {
-        const fetchCustomer = async () => {
-            try {
-                const res = await fetch("/api/customer", {
-                    cache: "no-store",
-                });
-
-                if (!res.ok) {
-                    throw new Error("Failed to fetch customer");
-                }
-
-                const response = await res.json();
-                setCustomerData(response.customers);
-            } catch (error) {
-                console.error("Error fetching customer:", error);
-            }
-        };
-
-        fetchCustomer();
-    }, []);
-
-
-    const filterSuppliesByKeyword = (supplies, keyword) => {
-        return supplies.filter((supply) => supply.supplyName.toLowerCase().includes(keyword));
-    };
-
-    useEffect(() => {
-        const fetchSupplies = async () => {
-            try {
-                const res = await fetch("/api/supply", {
-                    cache: "no-store",
-                });
-
-                if (!res.ok) {
-                    throw new Error("Failed to fetch supplies");
-                }
-
-                const response = await res.json();
-                setSupplyData(response.supplies);
-            } catch (error) {
-                console.error("Error fetching supplies:", error);
-            }
-        };
-
-        fetchSupplies();
-    }, []);
-
-    //filter detergent
-    const getDetergentSupplies = () => {
-        const detergentSupplies = filterSuppliesByKeyword(supplyData, 'detergent');
-        return detergentSupplies.map((supplies, i) => (
-            <option key={i}>{supplies.supplyName}</option>
-        ));
-    };
-
-    //filter fabcon
-    const getConditionerSupplies = () => {
-        const conditionerSupplies = filterSuppliesByKeyword(supplyData, 'conditioner');
-        return conditionerSupplies.map((supplies, i) => (
-            <option key={i}>{supplies.supplyName}</option>
-        ));
-    };
-
-
-    return (
-        <>
-            {isOpen && (
-                <div className="form-container visible">
-                    <div>
-                        <p id="header">Add New Laundry</p>
-                        <hr />
-                        <div className="customer-info">
-                            <p>Customer Name</p>
-                            <Autocomplete
-                                className="select-name"
-                                value={customerName}
-                                onChange={(event, newValue) => setCustomerName(newValue)}
-                                options={customerData.map((customer) => customer.customerName)}
-                                renderInput={(params) => <TextField {...params} variant="outlined" />}
-                            />
-                            <p>Date</p>
-                            <input
-                                type="date"
-                                value={orderDate}
-                                onChange={(e) => setOrderDate(e.currentTarget.value)}
-                            ></input>
-                        </div>
-                        <hr />
-                        <div className="form-group">
-                            <div id="first">
-                                <p>Weight</p>
-                                <select
-                                    value={weight}
-                                    onChange={(e) => setWeight(e.currentTarget.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="7.5Kg">Light Clothes - 7.5kilos</option>
-                                    <option value="8-9Kg">Light Clothes - 8 to 9 kilos</option>
-                                </select>
-                                <p>Fold</p>
-                                <select
-                                    value={fold}
-                                    onChange={(e) => setFold(e.currentTarget.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="Yes">Yes</option>
-                                    <option value="No">No</option>
-                                </select>
-                                <p>Detergent</p>
-                                <select
-                                    className="dropdown"
-                                    onChange={(e) => setDetergent(e.target.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="">None</option>
-                                    {getDetergentSupplies()}
-                                </select>
-                                <p>Detergent Qty.</p>
-                                <input
-                                    type="number"
-                                    value={detergentQty}
-                                    onChange={(e) => setDetergentQty(e.currentTarget.value)}
-                                ></input>
-                            </div>
-
-                            <div id="second">
-                                <p>Wash Mode</p>
-                                <select
-                                    value={washMode}
-                                    onChange={(e) => setWashMode(e.currentTarget.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="Spin">Spin - 9mins.</option>
-                                    <option value="Rinse & Spin">Rinse & Spin - 24mins.</option>
-                                    <option value="Regular Wash">Regular Wash – 37mins.</option>
-                                    <option value="Premium">Super/Premium Wash – 45mins.</option>
-                                </select>
-                                <p>Colored</p>
-                                <select
-                                    value={colored}
-                                    onChange={(e) => setColored(e.currentTarget.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="Yes">Yes</option>
-                                    <option value="No">No</option>
-                                </select>
-                                <p>Fabric Conditioner</p>
-                                <select
-                                    className="dropdown"
-                                    onChange={(e) => setFabCon(e.target.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="">None</option>
-                                    {getConditionerSupplies()}
-                                </select>
-                                <p>Fabric Conditioner Qty.</p>
-                                <input
-                                    type="number"
-                                    value={fabConQty}
-                                    onChange={(e) => setFabConQty(e.currentTarget.value)}
-                                ></input>
-                            </div>
-
-                            <div id="third">
-                                <p>Dry Mode</p>
-                                <select
-                                    value={dryMode}
-                                    onChange={(e) => setDryMode(e.currentTarget.value)}
-                                >
-                                    <option value=""></option>
-                                    <option value="30mins.">30mins.</option>
-                                    <option value="40mins.">40mins.</option>
-                                    <option value="50mins.">50mins.</option>
-                                </select>
-                                <p>Pay by:</p>
-                                <div className="radio-label">
-                                    <input
-                                        id="radiob"
-                                        type="radio"
-                                        name="paymentMethod"
-                                        value="Cash"
-                                        checked={paymentMethod === "Cash"}
-                                        onChange={(e) => setPaymentMethod(e.currentTarget.value)}
-                                    /> Cash
-                                    <input
-                                        id="radiob"
-                                        type="radio"
-                                        name="paymentMethod"
-                                        value="GCash"
-                                        checked={paymentMethod === "GCash"}
-                                        onChange={(e) => setPaymentMethod(e.currentTarget.value)}
-                                    />Gcash
-                                </div>
-                                <p>Ref. No. for Gcash</p>
-                                <input
-                                    type="text"
-                                    value={refNum}
-                                    onChange={(e) => setRefNum(e.currentTarget.value)}
-                                ></input>
-                            </div>
-
-                        </div>
-                        <br />
-                        <button className="cancel"
-                            onClick={onClose}
-                        >Cancel</button>
-                        <button className="save"
-                            onClick={onClick}
-                        >Save</button>
-                    </div>
-                </div>
-            )}
-        </>
-=======
   const filterSuppliesByKeyword = (supplies, keyword) => {
     return supplies.filter((supply) =>
       supply.supplyName.toLowerCase().includes(keyword)
->>>>>>> b3f645f4829de7be79e17a9414c11da13e90ef1e
     );
   };
 
