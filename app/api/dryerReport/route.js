346217323058e44c611e17e67eab242b1a4be52e
Levenie <ailalevenieacosta123@gmail.com>
2023-11-27 03:09:30 +0800

    added dryer report table to the db

---
+++ app/api/dryerReport/route.js
@@ -0,0 +1,38 @@
+import { connectToDB } from "@/utils/database";
+import DryerReport from "@/models/dryerReport";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectToDB();
+  const dryerReport = await DryerReport.find();
+  return NextResponse.json({ dryerReport });
+}
+
+export const POST = async (req) => {
+  const body = await req.json();
+  const { date, dryerNumber, useCount } = body;
+
+  try {
+    await connectToDB();
+    const newDryerReport = new DryerReport({
+      date,
+      dryerNumber,
+      useCount,
+    });
+    console.log(newDryerReport);
+    await newDryerReport.save();
+    return new Response(JSON.stringify(newDryerReport), { status: 201 });
+  } catch (error) {
+    return new Response(error, { status: 500 });
+  }
+};
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectToDB();
+  await DryerReport.findByIdAndDelete(id);
+  return NextResponse.json(
+    { message: "Delated a dryer report Record" },
+    { status: 201 }
+  );
+}
