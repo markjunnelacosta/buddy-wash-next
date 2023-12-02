428f7dba9e07747204dd65d81a7916257cb8c407
Levenie <ailalevenieacosta123@gmail.com>
2023-12-03 03:28:53 +0800

    branch2 laundry bin model & route

--- models/Branch2/Branch2Laundrybin.js
+++ models/Branch2/Branch2Laundrybin.js
@@ -1,64 +1,65 @@
 import { Schema, model, models } from "mongoose";
 
 const LaundryBinSchema = new Schema({
-    id: {
-        type: Schema.Types.ObjectId,
+  id: {
+    type: Schema.Types.ObjectId,
+  },
+  customerName: {
+    type: String,
+    required: [true, "Date is required"],
+  },
+  orderDate: {
+    type: Date,
+    required: [true, "Date is required"],
+    set: function (date) {
+      if (typeof date === "string") {
+        return new Date(date); // Parse the string as a Date
+      } else if (date instanceof Date) {
+        return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+      }
+      return date;
     },
-    customerName: {
-        type: String,
-        required: [true, "Date is required"]
-    },
-    orderDate: {
-        type: Date,
-        required: [true, "Date is required"],
-        set: function (date) {
-            if (typeof date === "string") {
-                return new Date(date); // Parse the string as a Date
-            } else if (date instanceof Date) {
-                return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-            }
-            return date;
-        },
-    },
-    weight: {
-        type: String,
-        required: [true, "Weight is required"]
-    },
-    washMode: {
-        type: String,
-        required: [true, "Wash Mode is required"]
-    },
-    dryMode: {
-        type: String,
-        required: [true, "Dry Mode is required"]
-    },
-    fold: {
-        type: String,
-    },
-    colored: {
-        type: String
-    },
-    detergent: {
-        type: String
-    },
-    fabcon: {
-        type: String
-    },
-    detergentQty: {
-        type: Number
-    },
-    fabconQty: {
-        type: Number
-    },
-    paymentMethod: {
-        type: String,
-        required: [true, "Payment Method is required"]
-    },
-    total: {
-        type: Number,
-    }
+  },
+  weight: {
+    type: String,
+    required: [true, "Weight is required"],
+  },
+  washMode: {
+    type: String,
+    required: [true, "Wash Mode is required"],
+  },
+  dryMode: {
+    type: String,
+    required: [true, "Dry Mode is required"],
+  },
+  fold: {
+    type: String,
+  },
+  colored: {
+    type: String,
+  },
+  detergent: {
+    type: String,
+  },
+  fabcon: {
+    type: String,
+  },
+  detergentQty: {
+    type: Number,
+  },
+  fabconQty: {
+    type: Number,
+  },
+  paymentMethod: {
+    type: String,
+    required: [true, "Payment Method is required"],
+  },
+  total: {
+    type: Number,
+  },
 });
 
-const LaundryBin = models.LaundryBin || model('LaundryBin', LaundryBinSchema);
+const Branch2LaundryBin =
+  models.Branch2LaundryBin || model("Branch2LaundryBin", LaundryBinSchema);
 
-export default LaundryBin; 
+export default Branch2LaundryBin;
