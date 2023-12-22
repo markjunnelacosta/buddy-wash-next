21ef05c913defcb63c14bd66b4e451b3c5c85742
Levenie <ailalevenieacosta123@gmail.com>
2023-12-22 12:33:16 +0800

    branch 3 order model and api

---
+++ app/api/BRANCH3/branch3Order/route.js
@@ -0,0 +1,94 @@
+import { connectToDB } from "@/utils/database";
+import Branch3Order from "@/models/Branch3/Branch3Order";
+import { NextResponse } from "next/server";
+import Machine from "@/models/machines";
+import Dryer from "@/models/dryers";
+
+// export const GET = async (req, res) => {
+//   try {
+//     await connectToDB();
+//     const customers = await Customer.find({}).populate("customerName");
+//     return new Response(JSON.stringify(customers), { status: 200 });
+//   } catch (error) {
+//     return new Response("Failed get customer", { status: 500 });
+//   }
+// };
+
+export async function GET() {
+  await connectToDB();
+  const orders = await Branch3Order.find().populate("machine");
+  return NextResponse.json({ orders });
+}
+
+export const POST = async (req) => {
+  const body = await req.json();
+  const {
+    date,
+    name,
+    machineNo,
+    machineAction,
+    machineTimer,
+    dryerNo,
+    dryerAction,
+    dryerTimer,
+    status,
+  } = body;
+
+  try {
+    await connectToDB();
+
+    const machine = await Machine.findOne({ machineNumber: machineNo });
+    const dryer = await Dryer.findOne({ dryerNumber: dryerNo });
+
+    if (!machine) {
+      return new Response("Machine not found", { status: 404 });
+    }
+    const newOrder = new Branch3Order({
+      date,
+      name,
+      machine: machine._id,
+      machineNo,
+      machineAction,
+      machineTimer,
+      dryer: dryer._id,
+      dryerNo,
+      dryerAction,
+      dryerTimer,
+      status,
+    });
+    console.log(newOrder);
+    await newOrder.save();
+    return new Response(JSON.stringify(newOrder), { status: 201 });
+  } catch (error) {
+    return new Response(error, { status: 500 });
+  }
+};
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectToDB();
+  await Branch3Order.findByIdAndDelete(id);
+  return NextResponse.json(
+    { message: "Deleted a customer Record" },
+    { status: 201 }
+  );
+}
+export async function PATCH(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  const body = await request.json();
+
+  try {
+    await connectToDB();
+
+    // Update the document with all fields from the request body
+    await Branch3Order.findByIdAndUpdate(id, body);
+
+    console.log(id);
+    return NextResponse.json(
+      { message: "Updated Order Table" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return new Response(error, { status: 500 });
+  }
+}
---
+++ models/Branch3/Branch3Order.js
@@ -0,0 +1,55 @@
+import { Schema, model, models } from "mongoose";
+
+const OrderSchema = new Schema({
+  id: {
+    type: Schema.Types.ObjectId,
+  },
+  date: {
+    type: String,
+    required: [true, "Date is required"],
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  machineNo: {
+    type: Number,
+    required: [true, "Machine Number is required"],
+  },
+  machineAction: {
+    type: String,
+    required: [false, "Action is not required"],
+  },
+  machineTimer: {
+    type: String,
+    required: [false, "Timer is not required"],
+  },
+  machine: {
+    type: Schema.Types.ObjectId,
+    ref: "Machine",
+  },
+  dryerNo: {
+    type: Number,
+    required: [true, "Dryer Number is required"],
+  },
+  dryerAction: {
+    type: String,
+    required: [false, "Action is not required"],
+  },
+  dryerTimer: {
+    type: String,
+    required: [false, "Timer is not required"],
+  },
+  dryer: {
+    type: Schema.Types.ObjectId,
+    ref: "Dryer",
+  },
+  status: {
+    type: String,
+    required: [false, "status is not required"],
+  },
+});
+
+const Branch3Order = models.Branch3Order || model("Branch3Order", OrderSchema);
+
+export default Branch3Order;
