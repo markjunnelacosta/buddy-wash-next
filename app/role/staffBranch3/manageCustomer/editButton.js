d677465cd273d2e3505974ed3e47df6b961f5221
Levenie <ailalevenieacosta123@gmail.com>
2023-12-22 14:03:36 +0800

    branch 3 - manage customers page

---
+++ app/role/staffBranch3/manageCustomer/editButton.js
@@ -0,0 +1,24 @@
+// EditUserPopup.js
+import React from "react";
+import UpdateCustomerForm from "../../components/forms/BRANCH3/updateCustomer/page";
+
+const EditCustomerPopup = ({ isOpen, customer, onClose }) => {
+  if (!isOpen) {
+    return null; // If not open, don't render anything
+  }
+
+  return (
+    <div className="popup-container">
+      <div className="popup">
+        <UpdateCustomerForm
+          id={customer._id}
+          customerName={customer.customerName}
+          customerNumber={customer.customerNumber}
+          onClose={onClose}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EditCustomerPopup;
