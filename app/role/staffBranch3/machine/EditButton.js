dd004c54d608cb3a4bceed5d0cdddd834838d97e
Levenie <ailalevenieacosta123@gmail.com>
2023-12-22 14:02:01 +0800

    branch 3 - machines page

---
+++ app/role/staffBranch3/machine/EditButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import UpdateMachine from '@/app/role/components/forms/updateMachine/page';
+import UpdateDryer from '@/app/role/components/forms/updateDryer/page';
+
+const EditPopup = ({ isOpen, item, onClose, type }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="popup-container">
+      <div className="popup">
+        {type === 'machine' ? (
+          <UpdateMachine
+            id={item._id}
+            machineNumber={item.machineNumber}
+            action={item.action}
+            timer={item.timer}
+            queue={item.queue}
+            useCount={item.useCount}
+            status={item.status}
+            onClose={onClose}
+          />
+        ) : (
+          <UpdateDryer
+            id={item._id}
+            dryerNumber={item.dryerNumber}
+            action={item.action}
+            timer={item.timer}
+            queue={item.queue}
+            useCount={item.useCount}
+            status={item.status}
+            onClose={onClose}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EditPopup;
