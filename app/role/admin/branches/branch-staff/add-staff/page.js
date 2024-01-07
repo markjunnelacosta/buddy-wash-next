3de03d65768ffc5a18bc5e4fd3e659a8a95f395c
Christine <christinebayas@gmail.com>
2024-01-07 23:43:34 +0800

    manager : owner position

--- app/role/admin/branches/branch-staff/add-staff/page.js
+++ app/role/admin/branches/branch-staff/add-staff/page.js
@@ -123,7 +123,7 @@ const StaffPage = ({ isOpen, onClose, onSaveData, branchId }) => {
                   onChange={(e) => setStaffPosition(e.currentTarget.value)}
                 >
                   <option value=""></option>
-                  <option value="Staff">Owner</option>
+                  <option value="Owner">Owner</option>
                   <option value="Staff">Staff</option>
                   <option value="Guard">Guard</option>
                   <option value="Technician">Technician</option>
